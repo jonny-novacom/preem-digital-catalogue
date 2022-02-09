85741cb602a3fc5c87ba5bdbbd60b56c37684829
jonny-novacom <58266470+jonny-novacom@users.noreply.github.com>
2022-02-09 09:15:47 +0000

    Add files via upload

--- src/components/Placeholder.js
+++ src/components/Placeholder.js
@@ -1,41 +1,15 @@
 import React from "react";
-import { graphql, useStaticQuery } from "gatsby";
-import { getImage } from "gatsby-plugin-image";
-
-import { convertToBgImage } from "gbimage-bridge";
-import BackgroundImage from "gatsby-background-image";
-
+import { StaticImage } from "gatsby-plugin-image";
 const PlaceholderImage = () => {
-  const { placeholderImage } = useStaticQuery(
-    graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "barrel.png" }) {
-          childImageSharp {
-            gatsbyImageData(
-              width: 200
-              placeholder: BLURRED
-              formats: [AUTO, WEBP, AVIF]
-            )
-          }
-        }
-      }
-    `
-  );
-  const image = getImage(placeholderImage);
-
-  // Use like this:
-  const bgImage = convertToBgImage(image);
-
   return (
-    <BackgroundImage
-      Tag="section"
-      // Spread bgImage into BackgroundImage:
-      {...bgImage}
-      preserveStackingContext
-      className="bg-center bg-no-repeat bg-auto"
-    >
-      <div className="h-60"></div>
-    </BackgroundImage>
+    <StaticImage
+      src="../images/barrel.png"
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="Marine"
+      transformOptions={"contain"}
+      className="block w-40 p-4 mx-auto mt-4 h-60"
+    />
   );
 };
 export default PlaceholderImage;
