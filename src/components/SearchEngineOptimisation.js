65ac20f892231dddb327a2a200764f8fd7430cf4
jonny-novacom <58266470+jonny-novacom@users.noreply.github.com>
2022-02-10 10:39:24 +0000

    Add files via upload

--- src/components/SearchEngineOptimisation.js
+++ src/components/SearchEngineOptimisation.js
@@ -42,8 +42,6 @@ export default function SearchEngineOptimisation({
         sizes="16x16"
         href="/favicon-16x16.png"
       />
-      <link rel="manifest" href="/site.webmanifest" />
-
       {/* Meta Tags */}
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta charSet="utf-8" />
