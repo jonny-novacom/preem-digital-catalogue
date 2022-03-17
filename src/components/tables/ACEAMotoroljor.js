3475f5f11be510899baa00e76faa39230aa1efae
jonny-novacom <58266470+jonny-novacom@users.noreply.github.com>
2022-03-17 11:21:04 +0000

    Add files via upload

--- src/components/tables/ACEAMotoroljor.js
+++ src/components/tables/ACEAMotoroljor.js
@@ -4,13 +4,14 @@ export default function ACEAMotoroljor() {
   return (
     <>
       <div>
-        <h3 className="text-center text-preemYellow text-2xl font-medium font-gothamNarrow">
-          ACEA motoroljor 2016
+        <h3 className="text-2xl font-medium text-center text-preemYellow font-gothamNarrow">
+          ACEA motoroljor 2021
         </h3>
-        <p className="text-left text-gray-500 text-md font-gothamNarrow font-normal mt-2 mb-4">
-          Bensin- och dieselmotorer i person- och lätta transportbilar.
+        <p className="mt-2 mb-4 font-normal text-left text-gray-500 text-md font-gothamNarrow">
+          A/B: Bensin- och dieselmotorer i person- och lätta transportbilar-
+          ”High SAPS”
         </p>
-        <table className="table-auto w-full shadow-none font-gothamNarrow font-medium overflow-x-scroll">
+        <table className="w-full overflow-x-scroll font-medium shadow-none table-auto font-gothamNarrow">
           <tbody>
             <tr className="border-b-8 border-black">
               <td className="text-left">Beteckning</td>
@@ -20,125 +21,151 @@ export default function ACEAMotoroljor() {
               <td>Bränslebesparing mot 15W-40%</td>
               <td>Beskrivning</td>
             </tr>
+
             <tr className="font-normal">
-              <td className="font-medium text-left pl-4">
-                A1/B1-12
+              <td className="pl-4 font-medium text-left">
+                A3/B3
                 <br />
-                <span className="font-normal">
-                  (utgående: uppdateras ej efter ACEA 2012)
-                </span>
+                <span className="font-normal text-red-500">Utgått!</span>
               </td>
-              <td>≥2,9 och ≤3,5; xW-20: min 2,6</td>
-              <td>≤1,3</td>
-              <td>High</td>
-              <td>≥2,5</td>
-              <td className="text-left">
-                Viskositetsstabil olja för förlängda bytesintervaller. För
-                motorer konstruerade för tunna lågfriktionsoljor. Olämplig för
-                vissa motortyper.
-              </td>
-            </tr>
-            <tr className="font-normal">
-              <td className="font-medium text-left pl-4">A3/B3</td>
               <td>≥3,5</td>
               <td>≥0,9 och ≤1,5</td>
               <td>High</td>
               <td>—</td>
               <td className="text-left">
-                Viskositetsstabil olja för högprestanda motorer och för vissa
-                motorer med förlängd bytesintervall eller hård belastning.
+                Viskositetsstabil motorolja med bra kvalitet, även gjord för
+                förlängda oljebytesintervall där det anges av
+                fordonstillverkaren.
               </td>
             </tr>
             <tr className="font-normal">
-              <td className="font-medium text-left pl-4">A3/B4</td>
+              <td className="pl-4 font-medium text-left">A3/B4</td>
               <td>≥3,5</td>
               <td>≥1,0 och ≤1,6</td>
               <td>High</td>
               <td>—</td>
               <td className="text-left">
-                Viskositetsstabil olja för högprestanda motorer och för vissa
-                motorer med förlängd bytesintervall eller hård belastning. För
-                direkt-insprutade dieselmotorer.
+                Viskositetsstabil motorolja med hög kvalitet avsedd för vissa
+                motorer med förlängt bytesintervall. Lämpar sig även för
+                direktinsprutade dieselmotorer. Kan även användas för fordon där
+                kravet är A3/B3.
               </td>
             </tr>
             <tr className="font-normal">
-              <td className="font-medium text-left pl-4">A5/B5</td>
+              <td className="pl-4 font-medium text-left">A5/B5</td>
               <td>≥2,9 och ≤3,5</td>
               <td>≤1,6</td>
               <td>High</td>
               <td>≥2,5</td>
               <td className="text-left">
-                Viskositetsstabil olja för förlängda bytesintervaller. För
-                högprestanda- motorer konstruerade för tunna lågfriktionsoljor.
-                Olämplig för vissa motortyper.
-              </td>
-            </tr>
-            <tr>
-              <td colSpan={6} className="font-medium text-left pl-4">
-                Oljor för motorer med trevägskatalysator och
-                dieselpartikelfilter
+                Viskositetsstabil motorolja med hög kvalitet avsedd för vissa
+                motorer med förlängt bytesintervall och motorer konstruerade för
+                tunna lågfriktionsoljor. Olämplig för vissa motortyper. Se
+                handboken vid tveksamheter!
               </td>
             </tr>
             <tr className="font-normal">
-              <td className="font-medium text-left pl-4">C1</td>
+              <td className="pl-4 font-medium text-left">A7/B7</td>
+              <td>≥2,9 och ≤3,5</td>
+              <td>≤1,6</td>
+              <td>High</td>
+              <td>≥2,5</td>
+              <td className="text-left">
+                Viskositetsstabil motorolja med hög kvalitet avsedd för vissa
+                motorer med förlängt bytesintervall och motorer konstruerade för
+                tunna lågfriktionsoljor. Skyddar även mot förtändning (LSPI) och
+                extra slitageskydd för turboladdade bensin- och dieselmotorer.
+                Se handboken vid tveksamheter!
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={6} className="pl-4 font-medium text-left">
+                Oljor för bensin- och dieselmotorer med trevägs katalysator och
+                partikelfilter, GPF/DPF
+              </td>
+            </tr>
+            <tr className="font-normal">
+              <td className="pl-4 font-medium text-left">
+                C1
+                <br />
+                <span className="font-normal text-red-500">Utgått!</span>
+              </td>
               <td>≥2,9</td>
               <td>≤0,5</td>
               <td>Low</td>
               <td>≥3,0</td>
               <td className="text-left">
-                Viskositetsstabil olja för hög-prestanda motorer konstruerade
-                för tunna lågfriktionsoljor. Lägst SAPS, olämplig för vissa
-                motortyper.
+                Viskositetsstabil motorolja för hög-prestanda motorer
+                konstruerade för tunna lågfriktionsoljor. Lägst SAPS, olämplig
+                för vissa motortyper.
               </td>
             </tr>
             <tr className="font-normal">
-              <td className="font-medium text-left pl-4">C2</td>
+              <td className="pl-4 font-medium text-left">C2</td>
               <td>≥2,9</td>
               <td>≤0,8</td>
               <td>Mid</td>
               <td>≥2,5</td>
               <td className="text-left">
-                Viskositetsstabil olja för hög-prestanda motorer konstruerade
-                för tunna lågfriktionsoljor. Olämplig för vissa motortyper.
+                Viskositetsstabil motorolja med hög kvalitet. Kompatibel med
+                efterbehandlingsutrustning. För motorer konstruerade för oljor
+                med HTHS-värde på min. 2.9 mPas, dvs lågfriktionsoljor. Olämplig
+                för vissa motortyper. Se handboken vid tveksamheter!
               </td>
             </tr>
             <tr className="font-normal">
-              <td className="font-medium text-left pl-4">C3</td>
+              <td className="pl-4 font-medium text-left">C3</td>
               <td>≥3,5</td>
               <td>≤0,8</td>
               <td>Mid</td>
               <td>≥1,0 (för xW-30)</td>
               <td className="text-left">
-                Viskositetsstabil olja för hög-prestandamotorer. Olämplig för
-                vissa motortyper.
+                Viskositetsstabil motorolja med hög kvalitet. Avsedd för
+                förlängda oljebytesintervaller i motorer som kräver ett
+                HTHS-värde på min. 3.5 mPas. Olämplig för vissa motortyper. Se
+                handboken vid tveksamheter!
               </td>
             </tr>
             <tr className="font-normal">
-              <td className="font-medium text-left pl-4">C4</td>
+              <td className="pl-4 font-medium text-left">C4</td>
               <td>≥3,5</td>
               <td>≤0,5</td>
               <td>Low</td>
               <td>≥1,0 (för xW-30)</td>
               <td className="text-left">
-                Viskositetsstabil olja för hög-prestandamotorer som kräver Low
-                SAPS. Olämplig för vissa motortyper.
+                Viskositetsstabil motorolja med hög kvalitet. Avsedd för
+                förlängda oljebytesintervaller i motorer som kräver ett
+                HTHS-värde på min. 3.5 mPas. Olämplig för vissa motortyper. Se
+                handboken vid tveksamheter!
               </td>
             </tr>
             <tr className="font-normal">
-              <td className="font-medium text-left pl-4">
-                C5
-                <br />
-                <span className="font-normal">(Ny i ACEA 2016)</span>
-              </td>
+              <td className="pl-4 font-medium text-left">C5</td>
               <td>≥2,6 och {"<"}2,9</td>
               <td>≤0,8</td>
               <td>Mid</td>
               <td>≥3,0</td>
               <td className="text-left">
-                Viskositetsstabil olja för hög-prestanda motorer konstruerade
-                för tunna lågfriktionsoljor. Lägsta HTHS, dvs höga krav på
-                bränslebesparing. Olämplig för vissa motortyper. Bygger på A1/B1
-                och C3, viskositet 0/5W-20.
+                Viskositetsstabil motorolja med hög kvalitet. Kompatibel med
+                efterbehandlingsutrustning. För motorer konstruerade för oljor
+                med HTHS-värde på min. 2.6 mPas, dvs lågfriktionsoljor. Olämplig
+                för vissa motortyper. Se handboken vid tveksamheter!
+              </td>
+            </tr>
+
+            <tr className="font-normal">
+              <td className="pl-4 font-medium text-left">C6</td>
+              <td>≥2,6 och {"<"}2,9</td>
+              <td>≤0,8</td>
+              <td>Mid</td>
+              <td>—</td>
+              <td className="text-left">
+                Viskositetsstabil motorolja med hög kvalitet. Kompatibel med
+                efterbehandlingsutrustning. För motorer konstruerade för oljor
+                med HTHS-värde på min. 2.6 mPas, dvs lågfriktionsoljor. .
+                Skyddar även mot förtändning (LSPI) och extra slitageskydd för
+                turboladdade bensin- och dieselmotorer. Se handboken vid
+                tveksamheter!
               </td>
             </tr>
           </tbody>
