4544225d2e26441818143acee49023094b5a6b98
jonny-novacom <58266470+jonny-novacom@users.noreply.github.com>
2022-03-30 14:39:45 +0100

    Add files via upload

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,17 +6,37 @@ export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
 }
 
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`
-  );
-  if (answer === true) {
-    window.location.reload();
-  }
-};
+// export const onRouteUpdate = () => {
+//   navigator.serviceWorker.register('/sw.js').then((reg) => {
+//     reg.update();
+//   });
+// };
 
-export const registerServiceWorker = () => true;
+// export const onServiceWorkerUpdateReady = () => {
+//   window.location.reload(true)
+// };
+
+// export const onServiceWorkerUpdateReady = () => {
+//   const answer = window.confirm(
+//     `This application has been updated. ` +
+//       `Reload to display the latest version?`
+//   );
+//   if (answer === true) {
+//     window.location.reload();
+//   }
+// };
+
+// export const registerServiceWorker = () => true;
 
 // // trigger an immediate page refresh when an update is found
 // export const onServiceWorkerUpdateReady = () => window.location.reload();
+
+exports.onServiceWorkerUpdateFound = () => {
+  if (
+    window.confirm(
+      "This site has been updated with new data. Do you wish to reload the site to get the new data?"
+    )
+  ) {
+    window.location.reload(true);
+  }
+};
