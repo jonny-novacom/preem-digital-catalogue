f007decd74a508e6ee7c18618fd91b417a2ba07b
jonny-novacom <58266470+jonny-novacom@users.noreply.github.com>
2022-06-17 09:36:35 +0100

    Add files via upload

--- src/components/SearchEngineOptimisation.js
+++ src/components/SearchEngineOptimisation.js
@@ -57,6 +57,13 @@ export default function SearchEngineOptimisation({
         key="ogsitename"
       />
       <meta property="og:description" content={description} key="ogdesc" />
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"
+        integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"
+      ></script>
+
       {children}
     </Helmet>
   );
